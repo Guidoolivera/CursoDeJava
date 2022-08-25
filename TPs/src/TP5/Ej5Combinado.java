2c2c56a477d2eb402a9e13af633ea6b7e3b457ff
Guidoolivera <guidoolivera1@gmail.com>
2022-08-24 23:02:27 -0300

    Ejercicios 4,5 y 6

---
+++ TPs/src/TP5/Ej5Combinado.java
@@ -0,0 +1,11 @@
+package TP5;
+
+public class Ej5Combinado {
+    public static void main(String[] args) {
+
+        double resultadoCombinado = ((1.2)-(.666*3.5)+(0.0666))/(0.333/5);
+
+        System.out.println(resultadoCombinado);
+
+    }
+}
---
+++ TPs/src/TP6/Ej6Funciones.java
@@ -0,0 +1,23 @@
+package TP6;
+
+public class Ej6Funciones {
+    public static void main (String[] args){
+
+
+    //Funciones trigonométricas habituales:
+
+        System.out.println(Math.sin(11.11));
+        System.out.println(Math.cos(30));
+        System.out.println(Math.tan(50.5));
+        System.out.println(Math.atan(32.32));
+        System.out.println(Math.atan2(2.2,4.4));
+
+    //La función exponencial y su inversa
+        System.out.println(Math.exp(4));
+        System.out.println(Math.log(6.3));
+
+    //Las dos constantes PI y e
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+    }
+}
