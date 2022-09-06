f3227ad787eeffab25c8c81aa11967b768e94dab
Guido Olivera <95640660+Guidoolivera@users.noreply.github.com>
2022-09-06 17:11:00 -0300

    Add files via upload

--- NUEVO/src/TPS/TP8.java
+++ NUEVO/src/TPS/TP8.java
@@ -1,4 +1,26 @@
 package TPS;
 
+import java.util.Scanner;
+
 public class TP8 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int numeroParaCalcular;
+        System.out.println("Ingrese un número para calcular su raíz: ");
+        numeroParaCalcular = scanner.nextInt();
+        RaizScanner raizScanner = new RaizScanner();
+        raizScanner.calcularRaiz(numeroParaCalcular);
+    }
+
+    public static class RaizScanner {
+
+        public void calcularRaiz(int numeroParaCalcular){
+
+            if (numeroParaCalcular > 0) {
+                System.out.println("La raíz de " + numeroParaCalcular + " es: " + Math.sqrt(numeroParaCalcular));
+            } else
+                System.out.println("Error: el número no puede ser negativo.");
+        }
+    }
 }
